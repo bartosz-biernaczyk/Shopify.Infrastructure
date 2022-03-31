76ebf27acc7ed338163101bfe37a7dc388e31c68
Bartosz Biernaczyk <bartoszbiernaczyk95@gmail.com>
2022-03-31 22:59:29 +0200

    Created simple abstraction over general registration & deregistration flow.

---
+++ Shopify.Consul/Models/ServiceDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopify.Consul.Models
+{
+    public class ServiceDetails
+    {
+        public string? ID { get; set; }
+
+    }
+}
---
+++ Shopify.Consul/Services/IConsulService.cs
@@ -0,0 +1,16 @@
+﻿using Shopify.Consul.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopify.Consul.Services
+{
+    public interface IConsulService
+    {
+        Task<HttpResponseMessage> RegisterAsync(ServiceDetails serviceDetails, CancellationToken token);
+        Task<HttpResponseMessage> DeregisterAsync(string serviceId, CancellationToken token);
+        Task<IDictionary<string, ServiceAgent>> ListServicesAsync(string name, CancellationToken token);
+    }
+}
